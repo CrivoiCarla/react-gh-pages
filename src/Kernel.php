0e6ee90b0959d1f207343d25e9c27c291f872b31
PetrisorTanasa <fptanasa02@yahoo.com>
2023-05-09 14:12:35 +0300

    Removed kernel modifications

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,15 +10,4 @@ use Symfony\Component\HttpFoundation\Response;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-    public function handle(Request $request, int $type = 1, bool $catch = true): Response
-    {
-        $response = parent::handle($request, $type, $catch);
-
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
-        $response->headers->set('Access-Control-Max-Age', '86400');
-
-        return $response;
-    }
 }
